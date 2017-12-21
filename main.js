077e49d310414a59d3bd8bd0ddbacd9473ba6d2b
Olli Kortelahti <ollikortelahti@Ollis-MacBook-Pro.local>
2017-12-21 02:08:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+const bb = require('bot-brother');
+let bot = bb({
+  key: '485597892:AAH5OM3gDIG-eqlGdWkwXJHyO5vuBgOYdvY',
+  sessionManager: bb.sessionManager.memory(),
+  polling: { interval: 0, timeout: 1 }
+});
+
+const axios = require('axios');
+
+// Let's create command '/start'.
+bot.command('start')
+.invoke(function (ctx) {
+  // Setting data, data is used in text message templates.
+  ctx.data.user = ctx.meta.user;
+  // Invoke callback must return promise.
+  return ctx.sendMessage('Hello <%=user.first_name%>. How are you?');
+})
+.answer(function (ctx) {
+  ctx.data.answer = ctx.answer;
+  // Returns promise.
+  return ctx.sendMessage('OK. I understood. You feel <%=answer%>');
+});
+
+// BTC
+bot.command('btc')
+.invoke(function (ctx) {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function (response) {
+        const args = ctx.command.args;
+
+        let value = 1; // defaults
+        let currency = 'BTC';
+
+        if(args[0]) {
+            const parsedValue = args[0];
+            if(parsedValue >= 0 && parsedValue <= 100000000) {
+                value = parsedValue;
+            }
+        }
+        if(args[1]) {
+            const parsedCurrency = args[1].toUpperCase();
+            if(['EUR', 'USD', 'GBP', 'BTC'].includes(parsedCurrency)) {
+                currency = parsedCurrency;
+            }
+        }
+
+        // X to BTC
+        if(['EUR', 'USD', 'GBP'].includes(currency)) {
+            const truevalue = (value / response.data.bpi[currency].rate_float).toFixed(8);
+
+            return ctx.sendMessage(
+                '*' + value + ' ' + currency + '* to *BTC*: `' + truevalue + '`',
+                {parse_mode: 'Markdown', disable_notification: true, reply_markup: {}}
+            );
+        }
+
+        // BTC to X
+        const finalusd = (value * response.data.bpi['USD'].rate_float).toFixed(2);
+        const finaleur = (value * response.data.bpi['EUR'].rate_float).toFixed(2);
+
+        return ctx.sendMessage(
+            '*' + value + ' BTC* to *USD*: `' + finalusd + '`\n' +
+            '*' + value + ' BTC* to *EUR*: `' + finaleur + '`', 
+            {parse_mode: 'Markdown', disable_notification: true, reply_markup: {}}
+        );
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+});
