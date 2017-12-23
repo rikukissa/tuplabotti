cc84030f05fd03c2f96596bb112234157192e28d
Riku Rouvila <riku.rouvila@gmail.com>
2017-12-23 22:57:08 +0000

    add eslint with airbnb's config, fix all warnings and errors from code

--- main.js
+++ main.js
@@ -4,26 +4,23 @@ const axios = require("axios");
 const interval = require("interval-promise");
 
 // Create bot object
-bot = bb({
+const bot = bb({
   key: keys.telegram,
   sessionManager: bb.sessionManager.memory(),
   polling: { interval: 0, timeout: 1 }
 });
 
 // List of supported currencies throughout the app
-supportedCurrencies = ["USD", "EUR", "GBP"];
-supportedCryptos = ["BTC", "ETH", "BCH"];
-supportedAll = supportedCurrencies.concat(supportedCryptos);
+const supportedCurrencies = ["USD", "EUR", "GBP"];
+const supportedCryptos = ["BTC", "ETH", "BCH"];
+const supportedAll = supportedCurrencies.concat(supportedCryptos);
 
 // Build API url
-apiUrl =
-  "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" +
-  supportedAll.join(",") +
-  "&tsyms=" +
-  supportedAll.join(",");
+const symbols = supportedAll.join(",");
+const apiUrl = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbols}&tsyms=${symbols}`;
 
 // Default message options
-messageOptions = {
+const messageOptions = {
   parse_mode: "Markdown",
   disable_notification: true,
   reply_markup: {}
@@ -31,45 +28,88 @@ messageOptions = {
 
 // Check notifications every 3 seconds
 // TODO: Change to greater interval, load notifications from file?
-notifications = {};
-
-interval(async () => {
-  await checkNotifications();
-}, 1000 * 3);
+let notifications = {};
 
 /*
-* Returns help text
-**/
+* Returns a string containing all current notifications
+*/
+function getNotifications(chatId) {
+  let currentlyNotifying = "*Currently notifying when:*\n";
+
+  // No notifications for current chat
+  if (notifications[chatId] === undefined) {
+    currentlyNotifying += "Never";
+
+    // Chat has notifications
+  } else if (notifications[chatId].length > 0) {
+    notifications[chatId].forEach(notification => {
+      currentlyNotifying += `${notification.crypto} ${notification.comparator}${
+        notification.rate
+      } ${notification.currency}\n`;
+    });
+
+    // Remove trailing \n
+    currentlyNotifying.slice(0, -1);
+  }
+
+  return currentlyNotifying;
+}
+
+/*
+ * Returns help text
+ */
+
 function getHelpText(command) {
-  if (command == "help") {
-    return "*Commands*" + "```\n/help [command]\n" + "/crypto\n" + "/notify```";
+  if (command === "help") {
+    return `*Commands* \`\`\`
+/help [command]
+/crypto
+/notify
+\`\`\``;
   }
 
   if (command === "crypto") {
-    return "*Usage:*\n" + "/crypto <amount> <from> to <to>\n" + "/crypto";
+    return `*Usage:*
+  /crypto <amount> <from> to <to>
+  /crypto`;
   }
 
   if (command === "notify") {
-    return (
-      "*Usage:*\n" +
-      "/notify <comparator><amount> <currency>\n" +
-      "/notify clear\n" +
-      "/notify\n\n" +
-      "*Example:* /notify btc >150 eur\n\n" +
-      getNotifications()
-    );
+    return `*Usage:*
+/notify <comparator><amount> <currency>
+/notify clear
+/notify
+
+*Example:* /notify btc >150 eur
+
+${getNotifications()}`;
   }
 
   return false;
 }
 
 function isNumber(num) {
-  return !isNaN(num);
+  return !Number.isNaN(num);
 }
 
 /*
-* Creates listeners for commands
-**/
+* Send notification message
+*/
+function showNotification(notification, currentRate) {
+  const overOrUnder = notification.comparator === ">" ? "over" : "under";
+
+  notification.ctx.sendMessage(
+    `*1 ${notification.crypto}* is now ${overOrUnder} *${notification.rate} ${
+      notification.currency
+    }*\n\n` +
+      `*1 ${notification.crypto}:* ${currentRate} ${notification.currency}`,
+    messageOptions
+  );
+}
+
+/*
+ * Creates listeners for commands
+ */
 
 /* bot.command('debug')
 .invoke(function (ctx) {
@@ -77,13 +117,14 @@ function isNumber(num) {
 }); */
 
 // Show help
-bot.command("help").invoke(function(ctx) {
-  const args = ctx.command.args;
+bot.command("help").invoke(ctx => {
+  const { args } = ctx.command;
 
   if (args.length === 0) {
     ctx.sendMessage(getHelpText("help"), messageOptions);
   }
-  if (args.length == 1) {
+
+  if (args.length === 1) {
     const text = getHelpText(args[0]);
 
     if (text) {
@@ -97,20 +138,20 @@ bot.command("help").invoke(function(ctx) {
 /*
 * Crypto command
 */
-bot.command("crypto").invoke(function(ctx) {
+bot.command("crypto").invoke(ctx => {
   axios
     .get(apiUrl)
-    .then(function(response) {
-      const args = ctx.command.args;
-      const data = response.data;
+    .then(response => {
+      const { args } = ctx.command;
+      const { data } = response;
 
       // No arguments
       if (args.length === 0) {
         let result = "";
 
         supportedCryptos.forEach(crypto => {
-          const usd = data[crypto]["USD"];
-          const eur = data[crypto]["EUR"];
+          const usd = data[crypto].USD;
+          const eur = data[crypto].EUR;
 
           result += `*1 ${crypto}* to *USD*: \`${usd}\`\n`;
           result += `*1 ${crypto}* to *EUR*: \`${eur}\``;
@@ -136,7 +177,7 @@ bot.command("crypto").invoke(function(ctx) {
           const fromCurrency = args[1].toUpperCase();
           const toCurrency = args[3].toUpperCase();
 
-          let result =
+          const result =
             `*${fromAmount} ${fromCurrency}* to *${toCurrency}*: ` +
             `\`${fromAmount * data[fromCurrency][toCurrency]}\``;
 
@@ -146,7 +187,8 @@ bot.command("crypto").invoke(function(ctx) {
         }
       }
     })
-    .catch(function(error) {
+    .catch(error => {
+      // eslint-disable-next-line no-console
       console.log(error);
     });
 });
@@ -154,25 +196,25 @@ bot.command("crypto").invoke(function(ctx) {
 /*
 * Notify command
 */
-bot.command("notify").invoke(function(ctx) {
+bot.command("notify").invoke(ctx => {
   axios
     .get(apiUrl)
-    .then(function(response) {
-      const args = ctx.command.args;
+    .then(() => {
+      const { args } = ctx.command;
 
       // Show list of current notifications
-      if (args.length == 0) {
+      if (args.length === 0) {
         ctx.sendMessage(getNotifications(), messageOptions);
-      } else if (args.length == 1) {
-        if (args[0] == "clear") {
-          notifications = [];
+      } else if (args.length === 1) {
+        if (args[0] === "clear") {
+          notifications = {};
           ctx.sendMessage("Cleared notifying list!", messageOptions);
         } else {
           ctx.sendMessage(getHelpText("notify"), messageOptions);
         }
 
         // Example: /notify btc >15000 eur
-      } else if (args.length == 3) {
+      } else if (args.length === 3) {
         const crypto = args[0].toUpperCase();
         const comparator = args[1][0];
         const rate = args[1].slice(1); // Slice removes < or > if there's any
@@ -186,7 +228,7 @@ bot.command("notify").invoke(function(ctx) {
           isNumber(rate)
         ) {
           const chatId = ctx.message.chat.id;
-          let notification = [];
+          const notification = [];
 
           notification.crypto = crypto;
           notification.comparator = comparator;
@@ -202,7 +244,7 @@ bot.command("notify").invoke(function(ctx) {
           notifications[chatId].push(notification);
 
           ctx.sendMessage(
-            "Added new notification!\n\n" + getNotifications(chatId),
+            `Added new notification!\n\n${getNotifications(chatId)}`,
             messageOptions
           );
         } else {
@@ -212,7 +254,8 @@ bot.command("notify").invoke(function(ctx) {
         ctx.sendMessage(getHelpText("notify"), messageOptions);
       }
     })
-    .catch(function(error) {
+    .catch(error => {
+      // eslint-disable-next-line no-console
       console.log(error);
     });
 });
@@ -221,14 +264,15 @@ bot.command("notify").invoke(function(ctx) {
 * Check notifications
 */
 function checkNotifications() {
-  if (Object.keys(notifications).length > 0) {
+  const chatIds = Object.keys(notifications);
+  if (chatIds) {
     return axios
       .get(apiUrl)
-      .then(function(response) {
-        const data = response.data;
+      .then(response => {
+        const { data } = response;
 
         // Loop through each chat
-        for (let chatId in notifications) {
+        chatIds.forEach(chatId => {
           // Loop through each notification
           notifications[chatId].forEach((notification, index, object) => {
             // Get current rate of crypto
@@ -251,56 +295,16 @@ function checkNotifications() {
               }
             }
           });
-        }
+        });
       })
-      .catch(function(error) {
+      .catch(error => {
+        // eslint-disable-next-line no-console
         console.log(error);
       });
   }
   return Promise.resolve();
 }
 
-/*
-* Send notification message
-*/
-function showNotification(notification, currentRate) {
-  const overOrUnder = notification.comparator === ">" ? "over" : "under";
-
-  notification.ctx.sendMessage(
-    `*1 ${notification.crypto}* is now ${overOrUnder} *${notification.rate} ${
-      notification.currency
-    }*\n\n` +
-      `*1 ${notification.crypto}:* ${currentRate} ${notification.currency}`,
-    messageOptions
-  );
-}
-
-/*
-* Returns a string containing all current notifications
-*/
-function getNotifications(chatId) {
-  let currentlyNotifying = "*Currently notifying when:*\n";
-
-  // No notifications for current chat
-  if (notifications[chatId] === undefined) {
-    currentlyNotifying += "Never";
-
-    // Chat has notifications
-  } else if (notifications[chatId].length > 0) {
-    notifications[chatId].forEach(notification => {
-      currentlyNotifying +=
-        notification.crypto +
-        " " +
-        notification.comparator +
-        notification.rate +
-        " " +
-        notification.currency +
-        "\n";
-    });
-
-    // Remove trailing \n
-    currentlyNotifying.slice(0, -1);
-  }
-
-  return currentlyNotifying;
-}
+interval(async () => {
+  await checkNotifications();
+}, 1000 * 3);
